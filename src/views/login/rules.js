ddca689e99a9523b208d049e3603f2cfb16f5087
ll <3462540450@qq.com>
2022-07-14 17:18:33 +0800

    fix: 商品下架层级问题

--- src/views/login/rules.js
+++ src/views/login/rules.js
@@ -2,6 +2,7 @@ import I18n from '@/i18n'
 
 export const validatePassword = () => {
   return (rule, value, callback) => {
+    console.log('nihao')
     if (value.length < 6) {
       callback(new Error(I18n.global.t('msg.login.passwordRule')))
     } else {
